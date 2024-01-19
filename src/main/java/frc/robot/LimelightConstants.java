632264cc865a90187e85f58bbd985bb958f356b7
Pedro Henrique <pedrohenrique01052006@gmail.com>
2024-01-19 15:18:59 -0300

    limelight adicionada

---
+++ src/main/java/frc/robot/LimelightConstants.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+public final class LimelightConstants {
+  
+  public static final String tableName = "limelight";
+  public static final int aprilTagPipeline = 0;
+  public static final int tapePipeline = 1;
+  public static final int driverPipeline = 2;
+
+  public static final class Seeking {
+    public static final double kP = -0.1;
+  }
+
+  public static final class AimTarget {
+    public static final double
+      kP = 0.06,
+      minCommand = 0.12;
+  }
+
+  public static final class RangeTarget {
+    public static final double 
+      kPDistance = -0.1;
+  }
+  
+  public static final class AimAndRangeTarget {
+    public static final double 
+      kP = -0.1,
+      kPDistance = -0.50,
+      minCommand = 0.05;
+  }
+}
