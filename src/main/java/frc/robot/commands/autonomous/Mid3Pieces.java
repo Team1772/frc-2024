9f5a57b10aa4cfc2aa6d532e906e067c78a9e330
Pedro Henrique <pedrohenrique01052006@gmail.com>
2024-02-22 11:54:04 -0300

    mid3pieces

--- src/main/java/frc/robot/commands/autonomous/Mid3Pieces.java
+++ src/main/java/frc/robot/commands/autonomous/Mid3Pieces.java
@@ -3,44 +3,31 @@ package frc.robot.commands.autonomous;
 import frc.core.util.TrajectoryBuilder;
 import frc.robot.commands.autonomous.Mid2Pieces.ForwardAndUpIntake;
 import frc.robot.commands.autonomous.Mid2Pieces.ReverseAndDownIntake;
-import frc.robot.commands.drivetrain.BackwardTimer;
-import frc.robot.commands.drivetrain.ForwardTimer;
-import frc.robot.commands.drivetrain.TurningToAngle;
 import frc.robot.commands.intake.CollectOff;
-import frc.robot.commands.intake.CollectOn;
-import frc.robot.commands.intake.ReleaseTimer;
 import frc.robot.commands.intake.ReleaseToShoot;
-import frc.robot.commands.intake_move.IntakeDownToSensor;
-import frc.robot.commands.intake_move.IntakeUpDown;
-import frc.robot.commands.intake_move.IntakeUpToSensor;
 import frc.robot.commands.shooter.ShootAutonomous;
 import frc.robot.commands.shooter.ShootOff;
-import frc.robot.commands.shooter.ShootOn;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.IntakeMove;
 import frc.robot.subsystems.Shooter;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class Mid3Pieces extends SequentialCommandGroup {
-
+// Max Acceleration = 1.1 Max Velocity = 1.1
   public Mid3Pieces(Drivetrain drivetrain, Shooter shooter, Intake intake, IntakeMove intakeMove,
       TrajectoryBuilder trajectoryBuilder) {
     super.addCommands(
-        new ShootAutonomous(1.5, intake, shooter),
+        new ShootAutonomous(1.8, intake, shooter),
         new ReverseAndDownIntake(intake, intakeMove, trajectoryBuilder, "1-reverse", true),
         new ForwardAndUpIntake(shooter, intake, intakeMove, trajectoryBuilder, "1-forward", false),
         new CollectOff(intake),
-        new WaitCommand(0.2),
         new ReleaseToShoot(intake, shooter, 1),
         new ShootOff(shooter),
         new ReverseAndDownIntake(intake, intakeMove, trajectoryBuilder, "1-reverse2Piece", false),
         new ForwardAndUpIntake(shooter, intake, intakeMove, trajectoryBuilder, "1-forward2Piece", false),
         new CollectOff(intake),
-        new WaitCommand(0.2),
         new ReleaseToShoot(intake, shooter, 1),
-        new ShootAutonomous(2, intake, shooter));
+        new ShootAutonomous(1.8, intake, shooter));
   }
 }
